64d02fb29bf79791e8805654c73eb26ec3e0973d
Anders Pearson <anders@thraxil.org>
2023-06-20 08:48:39 +0100

    add image component

--- lib/maya_web.ex
+++ lib/maya_web.ex
@@ -92,6 +92,7 @@ defmodule MayaWeb do
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
       import Phoenix.Component
+      import MayaWeb.Components.Image
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
