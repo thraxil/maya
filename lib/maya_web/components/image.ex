855253ca47d1445d3eec8a05e5a2884937fc9401
Anders Pearson <anders@thraxil.org>
2023-06-20 08:59:27 +0100

    switching to link

--- lib/maya_web/components/image.ex
+++ lib/maya_web/components/image.ex
@@ -1,18 +1,17 @@
 defmodule MayaWeb.Components.Image do
   use Phoenix.Component
   alias MayaWeb.Router.Helpers, as: Routes
-  import Phoenix.HTML.{Link,Tag}
+  import Phoenix.HTML.{Link, Tag}
   alias Maya.Portfolio.Image
 
   defp image_thumb_url(%Image{ahash: ahash, extension: extension, slug: slug}) do
     "https://d2f33fmhbh7cs9.cloudfront.net/image/" <> ahash <> "/200s/" <> slug <> extension
   end
 
-
   attr :height, :integer, default: 200
   attr :width, :integer, default: 200
   attr :image, Image, required: true
-  
+
   def image(assigns) do
     ~H"""
     <div class="image">
