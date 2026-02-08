eb939311cc4fd35d31d036b84895d1a266763fce
Anders Pearson <anders@thraxil.org>
2026-02-08 14:28:05 +0000

    Fix: Create priv/static directory before copying in release steps

--- mix.exs
+++ mix.exs
@@ -20,6 +20,7 @@ defmodule Maya.MixProject do
   end
 
   defp copy_static_assets(release) do
+    File.mkdir_p!(Path.join([release.path, "priv/static"]))
     File.cp_r!("priv/static", Path.join([release.path, "priv/static"]))
     release
   end
