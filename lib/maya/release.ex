0c264e329ac07ec7e7652c22e27b3e8fb63a9799
Anders Pearson <anders@thraxil.org>
2026-09-07 16:10:34 +0100

    deploy fix

--- lib/maya/release.ex
+++ lib/maya/release.ex
@@ -1,52 +1,28 @@
 defmodule Maya.Release do
   @moduledoc """
-  A module for release-related tasks.
+  Used for executing DB release tasks when run in production without Mix
+  installed.
   """
-
-  @repo Maya.Repo
-
-  def wait_for_db do
-    IO.puts("Waiting for database to be available...")
-
-    case {:persistent_term.get(:waited_for_db, false), System.get_env("RELEASE_COMMAND")} do
-      {true, _} ->
-        :ok
-
-      {_, "true"} ->
-        wait_for_db(0)
-
-      {_, _} ->
-        :ok
-    end
-  end
+  @app :maya
 
   def migrate do
-    IO.puts("Running migrations...")
+    load_app()
 
-    # Start the Ecto repo
-    {:ok, _} = Application.ensure_all_started(:ecto_sql)
-    {:ok, _} = Maya.Repo.start_link(pool_size: 2)
-
-    Ecto.Migrator.run(Maya.Repo, :up, all: true)
-
-    IO.puts("Migrations complete.")
-  end
-
-  defp wait_for_db(retries) when retries < 10 do
-    case @repo.start_link(pool_size: 1) do
-      {:ok, _pid} ->
-        :persistent_term.put(:waited_for_db, true)
-        :ok
-
-      {:error, _} ->
-        IO.puts("Database not available, waiting...")
-        :timer.sleep(500)
-        wait_for_db(retries + 1)
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
-  defp wait_for_db(_retries) do
-    IO.puts("Database not available after 10 retries, giving up.")
-    System.stop(1)
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
   end
 end
